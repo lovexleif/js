1eb4e8397838e0eb2935bf1330bff378bb518046
lei xie <lovexleif@163.com>
2017-06-05 23:29:02 +0800

    liyanhui_4_1_22017-06-0505_23:28:47

---
+++ 网易李言恢js第三章/3_1.js
@@ -0,0 +1,24 @@
+//var 是一个操作符  
+//var box  声明变量
+//var box =100  声明变量并初始化
+//alert(box)  以弹窗的方式输出
+
+
+//var  boxString="李炎恢";
+//boxString="lee"
+//alert(boxString);
+
+
+//作用域的问题
+//box="李炎恢"
+//alert(box);
+
+//语句在同一行  分号不能省略
+//语句在不同行分号省略不报错，但是建议每条后面都写分号
+//var box="李炎恢";var age=28;alert(box);alert(age);
+
+var box="李炎恢",
+       age=28,
+	   height=178,
+	   width;
+alert(width)   
---
+++ 网易李言恢js第四章/4_1.js
@@ -0,0 +1,80 @@
+
+/*
+var box;
+alert(typeof box);
+box 是Undefined类型，值是undefined，
+类型返回的字符串是undefined
+
+var box=true;
+alert(typeof box); //box是Boolean类型，值是true,类型返回的字符串是boolean
+
+var box="李炎恢";
+alert(typeof box); //box是String类型，值是'李炎恢',类型返回的字符串是string
+
+//空的对象表示这个对象创建了，里面没有东西
+//空对象表示没有创建，就是一个null
+var box={};
+alert(typeof box); //box是Object类型，值是[object Object],类型返回的字符串是object
+
+var box=null;
+alert(typeof box); //box是Object类型，值是null,类型返回的字符串是object
+
+var box=new Object();
+alert(typeof box); //box是Object类型，值是null,类型返回的字符串是object
+
+function box(){
+	document.write("sfash akh ");
+}
+alert(typeof box);//box是Funtion函数，值是function box{}，类型返回的字符串是function
+
+alert(typeof new object());
+
+//Udefined类型只有一个值，即特殊的undefined，在使用var声明变量，但没有对其初始化时，这个变量的值就是undefined
+//var box;alert(box);
+
+var box=undefined;//没有必要
+alert(box);
+var box;
+alert(typeof box);
+alert(typeof age);
+//一个应该时undefined一个时报错，结果都是undefined,以后要定义变量就要赋值，以避免这种错误。
+
+var box={};
+if (box!=null){//不等于null,说明不是空对象
+	alert("对象已近创建成功");
+}
+
+var box=null;//你还没有创建对象，但先声明了对象引用而必须初始化的结果
+                     //你还没来得及创建对象，先声明一个对象的变量放在那里，默认初始化为null
+
+var box=null;
+box={
+	1:1
+}
+alert(box);
+
+var box=''; //创建一个字符串变量，一开始不知道初始化什么字符串，所以就给他一个字符串，所以就给他一个空字符串初始化
+var box=0;//数值初始化，一般用0
+var box=false;//布尔值初始化，一般一开始用false或者true
+
+alert(undefined==null);//他们都是空的，所以相等
+
+alert(undefined===null);//数据类型也必须相等
+
+var box=true;
+alert(box===1);
+
+var box="";
+alert(Boolean(box));
+
+var box;
+if(box){//条件语句的()里必须是布尔值，true或者false
+	alert('真');
+}else{
+	alert('假');
+	
+//隐式转换
+}
+*/
+
+
